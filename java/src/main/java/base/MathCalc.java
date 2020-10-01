09661127fcddec75ea5c72ae51e5e2a389bdde7b
cpeixin <421391491@qq.com>
2020-10-01 14:48:50 +0800

    java case

---
+++ java/src/main/java/base/MathCalc.java
@@ -0,0 +1,24 @@
+package base;
+
+public class MathCalc {
+    public static void main(String[] args){
+
+        // int 4byte
+        // float 4byte
+
+        // double 8byte
+        // long 8byte
+
+        // char 1byte
+        byte byteVar = 127;
+        short shortVar = -30000;
+        int intVar = 30000000;
+        long bignum = 9999999999999L;
+        float floatVar = 100.1111111F;
+        double doubleVar = 100.11111111;
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+        System.out.println(1/3.0);
+
+    }
+}
